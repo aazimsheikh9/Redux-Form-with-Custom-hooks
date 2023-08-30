88df3df7ee9a8039482a59b724755290e99e0775
aazimsheikh9 <sheikhaazim13@gmail.com>
2023-08-31 01:39:18 +0530

    final

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     <>
       <h1 className='lg:text-5xl text-3xl text-center font-bold text-slate-700 py-10'>React Redux Form <br />With Custom Hooks</h1>
       <h3 className='lg:text-2xl text-lg text-center font-semibold text-slate-600 pt-10 pb-3'>Employment Form</h3>
+      
       <div className="flex justify-center">
         <Form />
       </div>
